5110cb95852e00b6c669a2b8cb054bf2be6fe9b1
ET <extazzy@gmail.com>
2021-06-05 10:24:20 +0300

    get balances

--- swapbot.py
+++ swapbot.py
@@ -10,11 +10,30 @@ from terra_sdk.core.wasm import MsgExecuteContract
 from terra_sdk.core.coins import Coins
 
 from time import sleep
-from pprint import pprint
+from pprint import pprint, pp
 from dotenv import load_dotenv
 load_dotenv()
 import os
 SEED = os.environ.get("SEED")
+MILLION = 1000000
+
+class BondedLunaToken:
+    bLunaContract = 'terra1kc87mu460fwkqte29rquh4hc20m54fxwtsx7gp'
+    
+    def __init__(self, terra, wallet=None) -> None:
+        self.terra = terra
+        self.wallet = wallet 
+
+    def get_balance(self):
+        result = self.terra.wasm.contract_query(
+            self.bLunaContract,
+            {
+                "balance": {
+                    "address": self.wallet.key.acc_address
+                }
+            }
+        )
+        return result   
 
 class TerraSwap:
     terraSwapContract = "terra1jxazgm67et0ce260kvrpfv50acuushpjsz2y0p"
@@ -30,7 +49,7 @@ class TerraSwap:
             {
                 "simulation": {
                     "offer_asset": {
-                        "amount": str(amount * 1000000),
+                        "amount": str(amount * MILLION),
                         "info": {
                             "native_token": {
                                 "denom": "uluna"
@@ -62,7 +81,7 @@ class TerraSwap:
             {
                 "swap": {
                     "offer_asset": {
-                        "amount": str(amount * 1000000),
+                        "amount": str(amount * MILLION),
                         "info": {
                             "native_token": {
                                 "denom": "uluna"
@@ -90,23 +109,45 @@ terra = LCDClient(chain_id="columbus-4", url="https://lcd.terra.dev")
 mk = MnemonicKey(mnemonic=SEED)
 wallet = terra.wallet(mk)
 swap = TerraSwap(terra, wallet)
+bLunaToken = BondedLunaToken(terra, wallet)
+# pp(bLunaToken.get_balance())
+num_bLunaTokens = (int) (bLunaToken.get_balance()['balance']) / MILLION
+pp(num_bLunaTokens)
 
-swap_amount = 1
-percent = 5
+swap_amount = 5
+buy_min_rate = 0.03
+sell_max_rate = 0.01
 
+coins = terra.bank.balance(wallet.key.acc_address)
+pp(coins)
+
+num_Luna = coins.get('uluna').amount / MILLION
+pp(num_Luna)
 # terra.tx.search()
 while True:
     rate = swap.get_exchange_rate(swap_amount)
-    rate['zProfit'] = int(rate['return_amount']) / 1000000 - swap_amount
+    rate['zProfit'] = int(rate['return_amount']) / MILLION - swap_amount
     rate['zzPR'] = '{:.2%}'.format(rate['zProfit'] / swap_amount)
     rate['zzzAPR'] = '{:.2%}'.format(rate['zProfit'] / swap_amount / 21 * 365)
     rate['zzzzAPY'] = '{:.2%}'.format((1 + rate['zProfit'] / swap_amount) ** 17 - 1)
     pprint(rate)
 
-    swap_result = swap.swap(swap_amount, rate['return_amount'])
-    pprint(swap_result)
+    if(rate['zProfit'] / swap_amount > buy_min_rate):
+        print('swap Luna -> bLuna')
+        if(num_Luna < swap_amount):
+            print('not enough Luna to swap')
+        else:
+            print('go swap')
+    #     swap_result = swap.swap(swap_amount, rate['return_amount'])
+    #     pprint(swap_result)
+    #     break
+    elif(rate['zProfit'] / swap_amount < sell_max_rate):
+        print('swap bLuna -> Luna')
+        if(num_bLunaTokens < swap_amount):
+            print('not enough bLuna to swap')
+        else:
+            print('go swap')
 
-
-    break
+    # break
     sleep(5)
 
